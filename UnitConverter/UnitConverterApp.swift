d87eef16a2e18ed809cde4deb0e7641b6f5d8604
Yasser Bal <yasserbal@icloud.com>
2023-09-04 20:59:13 +0200

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Yasser Bal on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
